be59520c995240e7883e2e9769eacf771265802d
Mar1o <mario12508@mail.ru>
2024-01-13 21:49:34 +0300

    test

--- test.py
+++ test.py
@@ -3,57 +3,60 @@ import sys
 
 pygame.init()
 
-# Определение цветов
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+# Размеры окна
+width, height = 400, 200
 
-# Инициализация экрана
-screen_width, screen_height = 800, 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Лесная локация")
+# Цвета
+black = (0, 0, 0)
+white = (255, 255, 255)
 
-# Загрузка изображения леса
-forest_image = pygame.image.load("forest_background.png")
+# Установка окна
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Volume Control')
 
-# Загрузка изображения объекта, в который можно отдать монету (сундук)
-chest_image = pygame.image.load("chest.png")
+# Загрузка звукового файла
+pygame.mixer.init()
+pygame.mixer.music.load('data/mus_town.ogg')  # Замените 'your_music_file.mp3' на свой файл
 
-# Позиция объекта
-object_x, object_y = 300, 400
+# Создание ползунка
+font = pygame.font.Font(None, 36)
+text = font.render('Volume:', True, white)
+text_rect = text.get_rect(center=(width // 2, height // 2 - 30))
 
-# Позиция монеты
-coin_x, coin_y = 50, 50
+volume = 0.5  # Начальная громкость
+volume_text = font.render(str(int(volume * 100)), True, white)
+volume_text_rect = volume_text.get_rect(center=(width // 2, height // 2 + 30))
 
-# Количество монет
-coin_count = 0
+pygame.mixer.music.set_volume(volume)  # Установка начальной громкости
+pygame.mixer.music.play(-1)  # Запуск воспроизведения с бесконечным циклом
 
-# Основной цикл игры
-while True:
+# Главный цикл
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            running = False
 
-        # Обработка действий при нажатии клавиши
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and coin_count > 0:
-                # Если нажата клавиша пробела и у игрока есть монета, отдать монету
-                coin_count -= 1
-                print("Монета отдана в сундук!")
+    # Обработка ползунка
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and volume < 1.0:
+        volume += 0.01
+    elif keys[pygame.K_DOWN] and volume > 0.0:
+        volume -= 0.01
 
-    # Отрисовка лесной локации
-    screen.blit(forest_image, (0, 0))
+    pygame.mixer.music.set_volume(volume)  # Обновление громкости
 
-    # Отрисовка объекта (сундук)
-    screen.blit(chest_image, (object_x, object_y))
+    # Обновление текста
+    volume_text = font.render(str(int(volume * 100)), True, white)
 
-    # Отрисовка монеты
-    if coin_count > 0:
-        pygame.draw.circle(screen, (255, 255, 0), (coin_x, coin_y), 15)
-
-    # Отображение количества монет
-    font = pygame.font.Font(None, 36)
-    text = font.render(f"Монеты: {coin_count}", True, WHITE)
-    screen.blit(text, (10, 10))
+    # Отрисовка
+    screen.fill(black)
+    screen.blit(text, text_rect)
+    screen.blit(volume_text, volume_text_rect)
 
     pygame.display.flip()
+
+    pygame.time.Clock().tick(60)
+
+pygame.quit()
+sys.exit()
